396de62dc83384e695facf662b5675cc828c8842
Jonathan Lu <jonathanlu321@gmail.com>
2023-08-25 15:06:44 -0400

    Code refactor

--- src/index.tsx
+++ src/index.tsx
@@ -2,81 +2,13 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-
-import * as esbuild from 'esbuild-wasm';
-import { useEffect } from 'react';
-import { useState } from "react";
-import { useRef } from 'react';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
-
-import CodeEditor from './components/code-editor';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
-  const [input, setInput] = useState("");
-
-  const startService = async() => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    });
-
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-  };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (e) => {
-            try {
-              eval(e.data);
-            } catch (error) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>';
-              console.error(error);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-    `;
-
   return (
-    <div className="App">
-      <CodeEditor initialValue='console.log("Type some code in!");' onChange={(value) => setInput(value)}/>
-      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
+    <div>
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 }
