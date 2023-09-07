36fa75c7436902154d4630d4b62baaf0312aab46
Jonathan Lu <jonathanlu321@gmail.com>
2023-09-07 16:54:03 -0400

    Cleaning up the code and removing unused

--- src/features/core/app.tsx
+++ src/features/core/app.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AphorismSingle from "../../features/aphorisms/aphorism-single";
+import AphorismSingle from "../aphorisms/aphorism-single";
 import Nav from "./nav";
-import AphorismList from "../../features/aphorisms/aphorism-list";
+import AphorismList from "../aphorisms/aphorism-list";
 import { useEffect } from "react";
-import { fetchAphorismsAsync } from "../../features/aphorisms/aphorismsSlice";
+import { fetchAphorismsAsync } from "../aphorisms/aphorismsSlice";
 import { useAppDispatch } from "../../state/hooks";
 
 const App = () => {
