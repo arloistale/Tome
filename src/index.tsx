c51b4b9ca1d6ea42aacb7793ce1d67ab56475727
Jonathan Lu <jonathanlu321@gmail.com>
2023-09-12 12:27:32 -0400

    Removing more unused code

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,15 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { store } from './state/store';
-
-import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './features/core/app';
 import { BrowserRouter } from 'react-router-dom';
-
-const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <Provider store={store}>
-    <QueryClientProvider client={client}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
