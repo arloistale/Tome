0ea433050de5e0585eaf8c275468e41dcefff59d
Jonathan Lu <jonathanlu321@gmail.com>
2023-09-12 13:49:10 -0400

    Fix issue with wrong date being displayed for when aphorism was presented

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,5 +1,6 @@
 export default function getDateString(date: Date) {
     const options: Intl.DateTimeFormatOptions = { 
+      timeZone: 'UTC', // so that the exact dates for presentation (in UTC) can be displayed correctly
       weekday: 'long', 
       year: 'numeric', 
       month: 'long', 
