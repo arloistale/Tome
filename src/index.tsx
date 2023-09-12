6a6214e581bed1d93bc28e2a0b2fbc3957c16737
Jonathan Lu <jonathanlu321@gmail.com>
2023-09-12 14:29:06 -0400

    Making it work again

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import 'bulmaswatch/simplex/bulmaswatch.min.css';
 
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 
 import { store } from './state/store';
 import App from './features/core/app';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <Provider store={store}>
