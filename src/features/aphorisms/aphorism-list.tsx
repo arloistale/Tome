0c29d523c4f9dd5c232f1af12ad40ab3ff8c98b2
Jonathan Lu <jonathanlu321@gmail.com>
2023-09-19 09:46:12 -0400

    Clean Up

--- src/features/aphorisms/aphorism-list.tsx
+++ src/features/aphorisms/aphorism-list.tsx
@@ -1,45 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAppSelector } from '../../state/hooks';
 import AphorismDisplay from './aphorism-display';
 import { presentedAphorisms } from './aphorismsSlice';
+import { Link, useLocation } from 'react-router-dom';
+
+const ITEMS_PER_PAGE = 5; 
+
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
 
 const AphorismList = () => {
-  const ITEMS_PER_PAGE = 5; // You can adjust this value based on your preference
-  const state = useAppSelector(state => state.aphorisms);
+  const { status, errorMessage } = useAppSelector(state => state.aphorisms);
   const presented = useAppSelector(presentedAphorisms);
 
-  const [currentPage, setCurrentPage] = useState(1);
   const totalPages = Math.ceil(presented.length / ITEMS_PER_PAGE);
 
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-      window.scrollTo(0, 0); 
-    }
-  };
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const page = clamp(Number(queryParams.get('page')) || 1, 1, totalPages);
 
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-      window.scrollTo(0, 0); 
-    }
-  };
-
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const startIndex = (page - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
   const displayedItems = presented.slice(startIndex, endIndex);
 
-  const renderedCells = displayedItems.map(item => (
-    <React.Fragment key={item.id}>
-      <AphorismDisplay item={item} />
-    </React.Fragment>
-  ));
+  const renderedCells = displayedItems.map(item => <AphorismDisplay key={item.id} item={item} />);
 
-  switch (state.status) {
+  switch (status) {
     case 'loading':
       return <p>Loading...</p>
     case 'failed':
-      return <p>Failed to load aphorisms: {state.errorMessage}</p>
+      return <p>Failed to load aphorisms: {errorMessage}</p>
     case 'idle':
       return (
         <div>
@@ -47,11 +38,11 @@ const AphorismList = () => {
             {renderedCells}
           </div>
           <nav className='pagination is-centered' role='navigation' aria-label="pagination">
-            {currentPage > 1 && (
-              <a className="pagination-previous" role="button" aria-label='Go to previous page' onClick={handlePreviousPage}>Previous</a>
+            {page > 1 && (
+              <Link className="pagination-previous" to={`/reflections?page=${page - 1}`}>Previous</Link>
             )}
-            {currentPage < totalPages && (
-              <a className="pagination-next" role="button" aria-label='Go to next page' onClick={handleNextPage}>Next</a>
+            {page < totalPages && (
+              <Link className="pagination-next" to={`/reflections?page=${page + 1}`}>Next</Link>
             )}
           </nav>
         </div>
