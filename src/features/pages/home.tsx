f8ab340aa3ac7c47f63f6a1ec0b79b9f32f61ba8
Jonathan Lu <jonathanlu321@gmail.com>
2023-10-06 15:27:28 -0400

    Get rid of the Today is

--- src/features/pages/home.tsx
+++ src/features/pages/home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
         <section className='hero is-primary'>
           <div className='hero-body'>
             <div className='container has-text-centered'>
-              <h1 className="subtitle is-3">Today is {getLongDateString(new Date())}</h1>
+              <h1 className="subtitle is-3">{getLongDateString(new Date())}</h1>
             </div>
           </div>
         </section>
