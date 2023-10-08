75a7342d4667df06a2a64070df716c29aa2e272b
Jonathan Lu <jonathanlu321@gmail.com>
2023-10-08 00:31:14 -0400

    Get rid of some selectors

--- src/features/core/nav.tsx
+++ src/features/core/nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <div className="navbar-brand">
           <Link className="navbar-item" to="/">
             <div className="container is-fluid">
-              <h1 className="title is-2 has-text-white">JONS REFLECTIONS</h1>
+              <h1 className="title is-3 py-2 has-text-white">JONS REFLECTIONS</h1>
             </div>
           </Link>
         </div>
