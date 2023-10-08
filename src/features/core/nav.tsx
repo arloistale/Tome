0c7c1eadab59a54726a5d95392f46675daad8309
Jonathan Lu <jonathanlu321@gmail.com>
2023-10-08 18:45:48 -0400

    Try this

--- src/features/core/nav.tsx
+++ src/features/core/nav.tsx
@@ -4,16 +4,14 @@ const Nav = () => {
     return (
       <nav className="main-nav navbar is-primary" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <Link className="navbar-item" to="/">
-            <div className="container is-fluid">
-              <h1 className="title is-3 py-2 has-text-white">JONS REFLECTIONS</h1>
-            </div>
-          </Link>
+          <div className="mx-auto">
+            <Link className="navbar-item py-2 title is-3 has-text-white" to="/">JONS REFLECTIONS</Link>
+          </div>
         </div>
         <div className="navbar-start">
-          <Link className="navbar-item" to="/">HOME</Link>
-          <Link className="navbar-item" to="/reflections">REFLECTIONS</Link>
-          <Link className="navbar-item" to="/about">ABOUT</Link>
+          <Link className="navbar-item has-text-centered" to="/">HOME</Link>
+          <Link className="navbar-item has-text-centered" to="/reflections">REFLECTIONS</Link>
+          <Link className="navbar-item has-text-centered" to="/about">ABOUT</Link>
         </div>
       </nav>
     );
