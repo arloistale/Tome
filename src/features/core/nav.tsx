f7edf979b0812e116c88085afce19ae47a0d7ccf
Jonathan Lu <jonathanlu321@gmail.com>
2023-10-08 18:58:16 -0400

    Putting container back and making it look nice

--- src/features/core/nav.tsx
+++ src/features/core/nav.tsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 const Nav = () => {
     return (
       <nav className="main-nav navbar is-primary" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <div className="mx-auto py-2">
-            <Link className="navbar-item title is-3 has-text-white" to="/">JONS REFLECTIONS</Link>
+        <div className="container">
+          <div className="navbar-brand">
+            <div className="mx-auto py-2">
+              <Link className="navbar-item title is-3 has-text-white" to="/">JONS REFLECTIONS</Link>
+            </div>
           </div>
-        </div>
-        <div className="navbar-end">
-          <Link className="navbar-item has-text-centered" to="/">HOME</Link>
-          <Link className="navbar-item has-text-centered" to="/reflections">REFLECTIONS</Link>
-          <Link className="navbar-item has-text-centered" to="/about">ABOUT</Link>
+          <div className="navbar-end">
+            <Link className="navbar-item has-text-centered" to="/">HOME</Link>
+            <Link className="navbar-item has-text-centered" to="/reflections">REFLECTIONS</Link>
+            <Link className="navbar-item has-text-centered" to="/about">ABOUT</Link>
+          </div>
         </div>
       </nav>
     );
