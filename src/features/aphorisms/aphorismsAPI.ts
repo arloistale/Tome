d56d70b5b943ec5ddd642328a87f77fa7ac937ec
Jonathan Lu <jonathanlu321@gmail.com>
2023-10-10 14:28:02 -0400

    Replace apollo with urql

--- src/features/aphorisms/aphorismsAPI.ts
+++ src/features/aphorisms/aphorismsAPI.ts
@@ -1,14 +1,13 @@
-import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import { Aphorism } from "./aphorism";
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { createClient, gql, cacheExchange, fetchExchange, TypedDocumentNode } from '@urql/core';
 import { isDev } from '../../util/is-dev';
 
 const productionApiUrl = "https://tome-backend.fly.dev/graphql";
 const api = isDev() ? "http://localhost:8000/graphql" : productionApiUrl;
 
-const client = new ApolloClient({
-  uri: api,
-  cache: new InMemoryCache(),
+const client = createClient({
+  url: api,
+  exchanges: [ cacheExchange, fetchExchange ]
 });
 
 const aphorismsQuery: TypedDocumentNode<{aphorisms: GraphQL_Aphorism[]}> = gql`
@@ -42,14 +41,13 @@ function deserializeAphorism(aphorism: GraphQL_Aphorism) {
 // TODO: In the future we may need to add a timezone parameter, to make sure that based on locale it only fetches
 // presented aphorisms up to the local date. In the current setup, at 8:05PM ETC on Sep 12 we will see the newest aphorism for Sep 13.
 export async function fetchAphorisms(): Promise<Aphorism[]> {
-  const result = await client.query({
-    query: aphorismsQuery
-  });
+  const result = await client.query(aphorismsQuery, {});
 
   if (result.error) {
     throw result.error;
   }
-
-  const converted = result.data.aphorisms.map(e => deserializeAphorism(e));
+  
+  const data = result.data!
+  const converted = data.aphorisms.map(e => deserializeAphorism(e));
   return converted;
 }
